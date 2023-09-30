bff2cbd91a7c107572828f53ad6fc8f430feb5a8
kiritocol <kiritomori@icloud.com>
2023-09-30 13:58:38 +0900

    ページとchatmemoの基礎mvc

---
+++ app/Http/Controllers/ChatmemoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Chatmemo;
+
+class ChatmemoController extends Controller
+{
+    public function top(Chatmemo $chatmemo)//インポートしたPostをインスタンス化して$postとして使用。
+{
+   return view('chatmemo.top')->with(['chatmemo' => $chatmemo->get()]);  
+}
+
+}
---
+++ resources/views/chatmemo/qpage.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qページ</title>
+    <style>
+        /* スタイル設定 */
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        /* 上部セクション */
+        .top-section {
+            background-color:  #f0f0f0;
+            height: 100px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-bottom: 1px solid #ccc;
+            position: relative; /* 相対位置に設定 */
+        }
+        .circle-back-button {
+            width: 45px;
+            height: 45px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 50%;
+            font-size: 20px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            position: absolute; /* 絶対位置に設定 */
+            top: 10px; /* 上部からの位置 */
+            left: 10px;
+        }
+        .circle-back-button:hover {
+            background-color: #ddd;
+        }
+        /* 真ん中セクション */
+        .middle-section {
+            text-align: center;
+            padding: 20px;
+
+            position: relative; /* 真ん中セクションを相対位置に設定 */
+        }
+
+        .speech-bubble {
+            background-color: #f0f0f0;
+            border-radius: 10px;
+            padding: 20px;
+            display: inline-block;
+            position: relative; /* 吹き出しを相対位置に設定 */
+        }
+
+        /* 下部セクション */
+        .bottom-section {
+            background-color:  #f0f0f0;
+            display: flex;
+            justify-content: center; /* ボタンを中央寄せに変更 */
+            align-items: flex-end;
+            height: 150px;
+            position: absolute;
+            bottom: 0px;
+            left: 0;
+            right: 0;
+            border-top: 1px solid #ccc;
+        }
+
+        .button {
+            width: 100px;
+            height: 100px;
+            background-color: #007bff;
+            color: #fff;
+            border-radius: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 10px;
+            cursor: pointer;
+        }
+
+        .button:hover {
+            background-color: #ddd;
+        }
+    </style>
+</head>
+<body>
+    <div class="top-section">
+        <a button class="circle-back-button" href="chatmemo2.html" >戻る</a>
+        </button>
+        会話
+    </div>
+    
+    <div class="middle-section">
+        <div class="speech-bubble">
+            今日は何について話しますか？
+            <div class="tail"></div>
+        </div>
+    </div>
+
+    <div class="bottom-section">
+        <a href="talk_memo.html"><div class="button">出来事</div></a>
+        <div class="button">予定</div>
+        <div class="button">やるべきこと</div>
+        <div class="button">考えたこと</div>
+    </div>
+</body>
+</html>
